5952a14421a81e079efeb46f343726f312a4e3e8
Sander Spiegelaar <sanderspiegelaar@gmail.com>
2024-06-25 11:39:54 +0200

    Card Modal & Header

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -3,6 +3,7 @@
 import { Draggable } from "@hello-pangea/dnd"
 
 import { Card } from "@prisma/client"
+import { useCardModal } from "@/hooks/use-card-modal"
 
 interface CardItemProps {
 	data: Card
@@ -10,6 +11,7 @@ interface CardItemProps {
 }
 
 export const CardItem = ({ data, index }: CardItemProps) => {
+	const cardModal = useCardModal()
 	return (
 		<Draggable
 			draggableId={data.id}
@@ -21,6 +23,7 @@ export const CardItem = ({ data, index }: CardItemProps) => {
 					{...provided.dragHandleProps}
 					ref={provided.innerRef}
 					role="button"
+					onClick={() => cardModal.onOpen(data.id)}
 					className="truncate border-2 border-transparent hover:border-black/20 py-2 px-3 text-sm bg-white rounded-md shadow-sm"
 				>
 					{data.title}
